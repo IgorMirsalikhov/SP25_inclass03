1bcd8d9bd192b962f13caf3743b00bff3def5375
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2025-04-02 15:34:09 -0400

    Update main.c

--- test/main.c
+++ test/main.c
@@ -1,1 +1,15 @@
 // Do not modify
+#include <stdio.h>
+
+bool isValidString(const char *str);
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <word>\n", argv[0]);
+        return 0;
+    }
+
+    printf("%d\n", isValidString(argv[1]));
+
+    return 0;
+}
